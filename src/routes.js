756b98c16c188807c69aa87e5c80d0af4308b1e1
Cleo Silva <cleo_silva1906@hotmail.com>
2022-08-19 20:56:45 -0300

    subindo pprojeto

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import Router from 'express';
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+const legendaryValidator = require('./middlewares/LegendaryValidator')
+
+const controller = require('./app/controllers/legendary/LegendaryController');
+const trainerController = require('./app/controllers/trainer/TrainerController')
+const uploadFileController = require('./app/controllers/utils/UploadFileController');
+const SessionController = require('./app/controllers/trainer/SessionController')
+
+const uploadFile = multer({ storage: multerConfig })
+
+const routes = new Router();
+
+//routes.get('/legendaries', controller.index);
+routes.get('/legendaries', controller.ListData);
+routes.post('/legendaries',legendaryValidator, controller.create);
+routes.put('/legendaries/:id', controller.update);
+routes.delete('/legendaries/:id', controller.delete);
+
+
+routes.get('/trainers', trainerController.listAll);
+routes.post('/trainers', trainerController.create);
+routes.put('/trainers/:id', trainerController.update);
+routes.post('/uploads', uploadFile.single('file'), uploadFileController.storeFile);
+routes.post('/session', SessionController.create);
+
+
+module.exports = routes;
